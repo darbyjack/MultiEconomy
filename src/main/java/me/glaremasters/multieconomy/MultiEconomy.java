f7d3232cd4a3ffe31c2d9c115b863d4faf265373
darbyjack <admin@glaremasters.me>
2018-05-24 15:09:35 -0500

    Started on first join event

--- src/main/java/me/glaremasters/multieconomy/MultiEconomy.java
+++ src/main/java/me/glaremasters/multieconomy/MultiEconomy.java
@@ -1,5 +1,10 @@
 package me.glaremasters.multieconomy;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import me.glaremasters.multieconomy.events.JoinEvent;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MultiEconomy extends JavaPlugin {
@@ -10,14 +15,29 @@ public final class MultiEconomy extends JavaPlugin {
         return i;
     }
 
+    public File dataFile = new File(this.getDataFolder(), "data.yml");
+    public YamlConfiguration dataFileConfig = YamlConfiguration.loadConfiguration(this.dataFile);
+
     @Override
     public void onEnable() {
         i = this;
         saveDefaultConfig();
+        saveData();
+
+        getServer().getPluginManager().registerEvents(new JoinEvent(i), this);
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    public void saveData() {
+        try {
+            dataFileConfig.save(dataFile);
+        } catch (IOException e) {
+            getLogger().log(Level.WARNING, "Could not save data file!");
+            e.printStackTrace();
+        }
     }
 }
