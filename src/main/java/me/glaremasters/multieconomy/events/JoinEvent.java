17e03982b5f1336e269c5757af78fd9180c60ddc
darbyjack <admin@glaremasters.me>
2018-05-24 22:06:03 -0500

    Handle adding in new economies

---
+++ src/main/java/me/glaremasters/multieconomy/events/JoinEvent.java
@@ -0,0 +1,32 @@
+package me.glaremasters.multieconomy.events;
+
+import me.glaremasters.multieconomy.MultiEconomy;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import sun.misc.UUDecoder;
+
+/**
+ * Created by GlareMasters on 5/24/2018.
+ */
+public class JoinEvent implements Listener {
+
+    private MultiEconomy i;
+
+    public JoinEvent(MultiEconomy i) {
+        this.i = i;
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        String UUID = event.getPlayer().getUniqueId().toString();
+        for (String type : i.getConfig().getStringList("economy-types")) {
+            if (i.dataFileConfig.get(UUID + "." + type) == null) {
+                i.dataFileConfig.set(UUID + "." + type, i.getConfig().getInt(type + ".start_amount"));
+            }
+        }
+        i.saveData();
+    }
+
+}
