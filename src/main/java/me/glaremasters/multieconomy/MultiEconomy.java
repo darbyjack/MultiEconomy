287db12bbeb26b3fcba78c421b33cb58c4ab9a0b
darbyjack <admin@glaremasters.me>
2018-05-28 02:27:20 -0500

    Added in announcements

--- src/main/java/me/glaremasters/multieconomy/MultiEconomy.java
+++ src/main/java/me/glaremasters/multieconomy/MultiEconomy.java
@@ -1,7 +1,11 @@
 package me.glaremasters.multieconomy;
 
+import static me.glaremasters.multieconomy.util.AnnouncementUtil.unescape_perl_string;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.logging.Level;
 import me.glaremasters.multieconomy.commands.CMDBalance;
 import me.glaremasters.multieconomy.commands.CMDBalances;
@@ -12,7 +16,10 @@ import me.glaremasters.multieconomy.commands.CMDPay;
 import me.glaremasters.multieconomy.commands.CMDReset;
 import me.glaremasters.multieconomy.commands.CMDSet;
 import me.glaremasters.multieconomy.commands.CMDTake;
+import me.glaremasters.multieconomy.events.AnnouncementListener;
 import me.glaremasters.multieconomy.events.JoinEvent;
+import org.apache.commons.io.IOUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -25,6 +32,7 @@ public final class MultiEconomy extends JavaPlugin {
     }
 
     public File dataFile = new File(this.getDataFolder(), "data.yml");
+
     public YamlConfiguration dataFileConfig = YamlConfiguration.loadConfiguration(this.dataFile);
 
     @Override
@@ -32,7 +40,7 @@ public final class MultiEconomy extends JavaPlugin {
         i = this;
         saveDefaultConfig();
         saveData();
-
+        Bukkit.getConsoleSender().sendMessage(getAnnouncements());
         getServer().getPluginManager().registerEvents(new JoinEvent(i), this);
         getCommand("mebalance").setExecutor(new CMDBalance(this));
         getCommand("meset").setExecutor(new CMDSet(this));
@@ -43,11 +51,41 @@ public final class MultiEconomy extends JavaPlugin {
         getCommand("mebalances").setExecutor(new CMDBalances(this));
         getCommand("mepay").setExecutor(new CMDPay(this));
         getCommand("mehelp").setExecutor(new CMDHelp());
+
+        getServer().getPluginManager().registerEvents(new AnnouncementListener(this), this);
+
+
+
     }
 
     @Override
     public void onDisable() {
 
+    }
+
+    /**
+     * Grab the announcement from the API
+     *
+     * @return announcement in string text form
+     */
+    public String getAnnouncements() {
+        String announcement = "";
+        try {
+            URL url = new URL("https://glaremasters.me/api/announcements/multieconomy/?id=" + getDescription()
+                    .getVersion());
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestProperty("User-Agent",
+                    "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
+            try (InputStream in = con.getInputStream()) {
+                String encoding = con.getContentEncoding();
+                encoding = encoding == null ? "UTF-8" : encoding;
+                announcement = unescape_perl_string(IOUtils.toString(in, encoding));
+                con.disconnect();
+            }
+        } catch (Exception exception) {
+            announcement = "Could not fetch announcements!";
+        }
+        return announcement;
     }
 
     public void saveData() {
---
+++ src/main/java/me/glaremasters/multieconomy/events/AnnouncementListener.java
@@ -0,0 +1,40 @@
+package me.glaremasters.multieconomy.events;
+
+import static me.glaremasters.multieconomy.util.ColorUtil.color;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+import me.glaremasters.multieconomy.MultiEconomy;
+import me.rayzr522.jsonmessage.JSONMessage;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+/**
+ * Created by GlareMasters on 5/28/2018.
+ */
+public class AnnouncementListener implements Listener {
+
+    private Set<UUID> ALREADY_INFORMED = new HashSet<>();
+
+    private MultiEconomy multiEconomy;
+
+    public AnnouncementListener(MultiEconomy multiEconomy) {
+        this.multiEconomy = multiEconomy;
+    }
+
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+            multiEconomy.getServer().getScheduler().scheduleAsyncDelayedTask(multiEconomy, () -> {
+                if (player.isOp()) {
+                    if (!ALREADY_INFORMED.contains(player.getUniqueId())) {
+                        JSONMessage.create(color(multiEconomy.getConfig().getString("plugin-prefix") + " &fAnnouncements")).tooltip(multiEconomy.getAnnouncements()).openURL(multiEconomy.getDescription().getWebsite()).send(player);ALREADY_INFORMED.add(player.getUniqueId());
+                    }
+                }
+            }, 70L);
+    }
+
+}
+
