71bc2bf3ee2c2b17dd1307eb2e0b372ed54374a4
darbyjack <admin@glaremasters.me>
2018-05-29 18:26:53 -0500

    Finished up leaderboard

--- src/main/java/me/glaremasters/multieconomy/commands/CMDTop.java
+++ src/main/java/me/glaremasters/multieconomy/commands/CMDTop.java
@@ -3,16 +3,17 @@ package me.glaremasters.multieconomy.commands;
 import static me.glaremasters.multieconomy.api.API.checkArgs;
 import static me.glaremasters.multieconomy.api.API.checkEcoType;
 import static me.glaremasters.multieconomy.api.API.checkPerms;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map.Entry;
 import java.util.UUID;
-import java.util.Vector;
 import me.glaremasters.multieconomy.MultiEconomy;
-import me.glaremasters.multieconomy.util.TopUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
 /**
@@ -28,6 +29,7 @@ public class CMDTop implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        YamlConfiguration dFC = multiEconomy.dataFileConfig;
         if (!checkArgs(sender, args, 1, "metop")) return true;
         if (sender instanceof Player) {
             Player player = (Player) sender;
@@ -35,24 +37,20 @@ public class CMDTop implements CommandExecutor {
             String econType = args[0].toLowerCase();
             if (!checkEcoType(sender, econType)) return true;
 
-            Map<String, Object> list = new TreeMap<>();
+            HashMap<String, Integer> data = new HashMap<>();
 
-            for (String key : multiEconomy.dataFileConfig.getKeys(false)) {
-                list.put(key, multiEconomy.dataFileConfig.get(key + "." + econType));
+
+            for (String key : dFC.getKeys(false)) {
+                data.put(key, Integer.valueOf(dFC.get(key + "." + econType).toString()));
             }
-            TopUtil topUtil = new TopUtil(list);
-            Map<String, Object> newMap = new TreeMap(topUtil);
-            newMap.putAll(list);
 
-            StringBuilder sb = new StringBuilder();
-
-            for (int i = 0; i < newMap.size(); i++) {
-                String ID = new Vector(newMap.keySet()).get(i).toString();
+            List<Entry<String, Integer>> list = new LinkedList<>(data.entrySet());
+            list.sort((o1, o2) -> o2.getValue() - o1.getValue());
+            for (int i = 0; i < dFC.getKeys(false).size(); i++) {
+                String ID = list.get(i).getKey();
                 String name = Bukkit.getServer().getOfflinePlayer(UUID.fromString(ID)).getName();
-                String value = new Vector(newMap.values()).get(i).toString();
-                sb.append("\n" + name + " - " + value);
+                Bukkit.broadcastMessage(name + " - " + list.get(i).getValue());
             }
-            player.sendMessage(sb.toString());
 
         }
         return true;
